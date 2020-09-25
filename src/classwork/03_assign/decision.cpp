2fb88fa5e2f9ab439e14e2976f677bdc16e66c12
djchenevert <david.chenevert1@g.austincc.edu>
2020-09-25 16:43:32 -0500

    letter grade if switch

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,70 @@
 //cpp
+#include "decision.h"
+using std::cout;
+using std::string;
+using std::cin;
+
+
+string get_letter_grade_using_if(int grade)
+{
+    string letter;
+
+    if(grade >= 90 && grade <= 100)
+    {
+        letter = "A";
+        cout<<"Your letter grade is an "<<letter<<"\n";
+    }
+        else if(grade >= 80 && grade <= 89)
+        {
+            letter = "B";
+            cout<<"Your letter grade is an "<<letter<<"\n";
+        }
+        else if(grade >= 70 && grade <= 79)
+        {
+            letter = "C";
+            cout<<"Your letter grade is an "<<letter<<"\n";
+        }
+        else if(grade >= 60 && grade <= 69)
+        {
+            letter = "D";
+            cout<<"Your letter grade is an "<<letter<<"\n";
+        }
+        else if(grade >= 00 && grade <= 59)
+        {
+            letter = "F";
+            cout<<"Your letter grade is an "<<letter<<"\n";
+        }
+        
+    return letter;
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+    string letter_grade;
+
+    switch (grade)
+    {
+    case 90 ... 100:
+        letter_grade = "A";
+        cout<<"Your letter grade is an "<<letter_grade<<"\n";
+        break;
+    case 80 ... 89:
+        letter_grade = "B";
+        cout<<"Your letter grade is an "<<letter_grade<<"\n";
+        break;
+    case 70 ... 79:
+        letter_grade = "C";
+        cout<<"Your letter grade is an "<<letter_grade<<"\n";
+        break;
+    case 60 ... 69:
+        letter_grade = "D";
+        cout<<"Your letter grade is an "<<letter_grade<<"\n";
+        break;
+    case 00 ... 59:
+        letter_grade = "F";
+        cout<<"Your letter grade is an "<<letter_grade<<"\n";
+        break;
+    }
+
+    return letter_grade;
+}
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,9 +1,27 @@
 //Write the include statement for decisions.h here
+#include "decision.h"
 
 //Write namespace using statements for cout and cin
+using std::cin;	using std::cout;
 
 int main() 
 {
+	int grade;
+
+	cout<<"Input your numerical grade between 0 and 100: ";
+	cin>>grade;
+
+	if(grade >= 00 && grade <= 100)
+	{
+		get_letter_grade_using_if(grade);
+
+		get_letter_grade_using_switch(grade);
+
+	}
+		else
+		{
+			cout<<"You have entered an invalid number.";
+		}
 
 	return 0;
 }
