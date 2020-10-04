f344c8993bff7354e3d12d863db41a2b50ffe4c7
djchenevert <david.chenevert1@g.austincc.edu>
2020-10-03 20:00:10 -0500

    commit do while factorial loop

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,15 @@
 //cpp
+#include "loops.h"
+using std::cout;
+int factorial(int num)
+{
+    int fact = 1;
+    int i;
+    for (int i = 1; i <= num; i++)
+    {
+        fact = fact * i;
+    }
+    cout<<"The factorial is "<<fact;
+
+    return fact;
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,8 +1,7 @@
 //write includes statements
-
+#include "loops.h"
 //write using statements for cin and cout
-
-
+using std::cout;	using std::cin;
 /*
 Use a do while loop to prompt the user for 
 a number, call the factorial function, and display the number's
@@ -10,6 +9,12 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
-
-	return 0;
+	int num;
+	cout<<"Enter a number to get the factorial of the number: ";
+	cin>> num;
+	do{
+		factorial(num);
+		return 0;
+	}
+	while(num >0 && num <= 10);
 }
