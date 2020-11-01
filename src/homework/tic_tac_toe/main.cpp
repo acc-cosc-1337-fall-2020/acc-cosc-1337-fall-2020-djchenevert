059f48204eed345f4229095a585e725936cd140a
djchenevert <david.chenevert1@g.austincc.edu>
2020-11-01 17:38:36 -0600

    push

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,30 @@
+#include"tic_tac_toe.h"
+
+using std::cout;	using std::cin;
 
 int main() 
 {
-
+	TicTacToe start;
+	cout<<"Shall we play a game? Y or N: ";
+	char guess;
+	cin>>guess;
+	while (guess == 'Y' || guess == 'y')
+	{
+		cout<<"Enter X or O: ";
+		string value;
+		cin>>value;
+		while(start.game_over() != true)
+		{
+			start.start_game(value);
+			int position;
+			cout<<"Enter a position from 1 to 9: ";
+			cin>>position;
+			start.mark_board(position);
+			start.display_board();
+		}
+		cout<<"Shall we play a game? Y or N: ";
+		cin>>guess;
+	}	
+		
 	return 0;
 }
--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,69 @@
-//cpp
+#include "tic_tac_toe.h"
+
+using std::cout;    using std::cin;
+
+bool TicTacToe::game_over()
+{
+    return check_board_full();
+}
+void TicTacToe::start_game(string first_player)
+{
+    player = first_player;
+    clear_board();
+}
+void TicTacToe::mark_board(int position)
+{
+    int new_position = position - 1;
+    pegs[new_position] = player;
+    set_next_player();
+}
+string TicTacToe::get_player() const
+{
+    return player;
+}
+void TicTacToe::display_board() const
+{
+    for(int i = 0; i < pegs.size(); i++)
+    {
+        if( i == 2 || i == 5 || i == 8){
+            cout <<pegs[i]<<"\n";
+       } else{
+            cout <<pegs[i]<<"|";
+        }
+       
+    }
+    cout<<"\nEND";
+}
+//Private
+void TicTacToe::set_next_player()
+{
+    if(player == "X" || player == "x")
+    {
+        cout<<("penguins\n");
+        player = "O";
+    }
+    else
+    {
+        cout<<"giraffe\n";
+        player = "X";
+    }
+    cout<<"player == "<<player<<"\n";
+}
+bool TicTacToe::check_board_full()
+{
+    for(int i = 0; i < pegs.size(); i++)
+    {
+        if(pegs[i] == " ")
+        {
+            return false;
+        }
+    }
+    return true;
+}
+void TicTacToe::clear_board()
+{
+    for(int i = 0; i < pegs.size(); i++)
+    {
+        pegs[i] = " ";
+    }
+}
